69f593526bffb55bb6403ad7545d2782f8bce74b
Nicolas Sousa <nicollassouza141@gmail.com>
2024-03-27 16:19:56 -0300

    first commit

--- src/constainers/Projetos/index.tsx
+++ src/constainers/Projetos/index.tsx
@@ -11,63 +11,56 @@ const Projetos = () => {
   }, [])
   const projetosData = [
     {
+      titulo: 'Eplay',
+      descricao: 'Landing page de games com Api e usando react.',
+      link: 'https://github.com/nicolas-sou/Eplay'
+    },
+    {
       titulo: 'Efood',
-      descricao: 'Landing page de restaurante com as Api e react.',
+      descricao: 'Landing page de restaurante com Api e usando react.',
       link: 'https://github.com/nicolas-sou/Efood'
     },
     {
-      titulo: 'Lista de tarefas',
-      descricao: 'Uma landing page lista de tarefas com Vue.',
-      link: 'https://github.com/nicolas-sou/Lista_Tarefas_VueJS'
-    },
-    {
-      titulo: 'Landing page aniversário',
-      descricao: 'Convite de aniversaio.',
-      link: 'https://github.com/nicolas-sou/landing_page_aniversario'
-    },
-    {
-      titulo: 'Projeto filme',
-      descricao: 'Uma landing page sobre o filme usando scss.',
-      link: 'https://github.com/nicolas-sou/site_filme/blob/main/index.html'
-    },
-    {
-      titulo: 'Clone do disneyplus',
-      descricao: 'Uma landing page do disneyplus com scss.',
-      link: 'https://github.com/nicolas-sou/clone_disneyplus'
-    },
-    {
-      titulo: 'Sorteador',
-      descricao: 'Uma landing page sorteador com grunt e less.',
-      link: '/caminho/do/seu/projeto2'
-    },
-    {
-      titulo: 'Projeto restaurante',
-      descricao: 'Uma landing page de restaurante usando bootstrap.',
+      titulo: 'Site restaurante',
+      descricao: 'Landing page usando bootstrap.',
       link: 'https://github.com/nicolas-sou/Site-restaurante'
     },
     {
+      titulo: 'Projeto filme',
+      descricao: 'Landing page sobre um filme usando scss.',
+      link: 'https://github.com/nicolas-sou/site_filme'
+    },
+    {
+      titulo: 'Clone do disneyplus',
+      descricao: 'Landing page do disneyplus com scss.',
+      link: 'https://github.com/nicolas-sou/clone_disneyplus'
+    },
+    {
+      titulo: 'Ebac Jobs',
+      descricao: 'landing page sobre vagas de trabalho usando react',
+      link: 'https://github.com/nicolas-sou/base_exercicio_css_in_js'
+    },
+    {
+      titulo: 'Projeto loja tênis',
+      descricao: 'Landing page usando Html e Css.',
+      link: 'https://github.com/nicolas-sou/loja_virtual'
+    },
+    {
       titulo: 'Site games shop',
-      descricao: 'Uma landing page simples.',
+      descricao: 'Landing page usando Html e Css.',
       link: 'https://github.com/nicolas-sou/site_gamesshop'
     },
     {
       titulo: 'Projeto Ebac motors',
-      descricao:
-        'Uma landing page sobre venda de carros usando plugins jquery.',
+      descricao: 'Landing page sobre venda de carros usando plugins jquery.',
       link: 'https://github.com/nicolas-sou/exerc-cio_plugins_jquery'
-    },
-    {
-      titulo: 'Projeto clínica',
-      descricao:
-        'Uma landing page de uma clinica de reabilitação usando bootstrap.',
-      link: 'https://github.com/nicolas-sou/Site_clinica-2'
     }
   ]
   return (
     <section
       data-aos="fade-down"
       data-aos-easing="linear"
-      data-aos-duration="1500"
+      data-aos-duration="500"
     >
       <Titulo fontSize={16}>Projetos</Titulo>
       <Lista>
@@ -76,7 +69,7 @@ const Projetos = () => {
             key={index}
             data-aos="fade-down"
             data-aos-easing="linear"
-            data-aos-duration="1500"
+            data-aos-duration="300"
           >
             <Projeto
               titulo={projeto.titulo}
